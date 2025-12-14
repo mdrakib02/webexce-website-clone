59fd5cca7bd8ee74e65a40197934d71424cabbdb
mdrakib02z <mdrakibahmed515@gmail.comm>
2025-12-14 02:10:34 -0500

    update compenenst

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+
 import { AnimatedCursor } from "@/components/animated-cursor";
 import { ContactSection } from "@/components/contact-section";
 import { Footer } from "@/components/footer";
@@ -10,6 +11,7 @@ import { ScrollHeader } from "@/components/scroll-header";
 import { ServicesSection } from "@/components/service-section";
 import { SolutionSection } from "@/components/solution-section";
 import { TrustSection } from "@/components/trust-section";
+
 
 
 export default function Home() {
